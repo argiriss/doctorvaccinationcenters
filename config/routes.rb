9f21e5252fc47879cc661cf1ad290163bbcc6649
Ar An <anastasakis.an@gmail.com>
2021-05-08 11:14:07 +0300

    Root to doctors index

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   resources :doctors
   resources :vaccination_centers
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root "doctors#index"
 end
